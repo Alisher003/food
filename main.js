7278922a78ac7ede8a345eb2739a0bff02777e8e
Alisher003 <abduvaliyevalisher003@gmail.com>
2022-02-16 22:39:01 +0500

    home section

---
+++ main.js
@@ -0,0 +1,14 @@
+let menu = document.querySelector('#menu-bar')
+let navbar = document.querySelector('.navbar')
+
+menu.onclick = () =>{
+
+    menu.classList.toggle('fa-times')
+    navbar.classList.toggle('active')
+}
+
+window.onscroll = () => {
+
+    menu.classList.remove('fa-items')
+    navbar.classList.remove('active')
+}
