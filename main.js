bae249c96262f4be0832486042138a4cbf0be235
Alisher003 <abduvaliyevalisher003@gmail.com>
2022-02-20 14:52:14 +0500

    finished

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
-let menu = document.querySelector('#menu-bar')
-let navbar = document.querySelector('.navbar')
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
-
-    menu.classList.toggle('fa-times')
-    navbar.classList.toggle('active')
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
 }
 
 window.onscroll = () => {
-
-    menu.classList.remove('fa-items')
-    navbar.classList.remove('active')
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    if(window.scrollY > 700){
+        document.querySelector('#scroll-top').classList.add('active')
+    }else{
+        document.querySelector('#scroll-top').classList.remove('active')
+    }
 }
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out')
+}
+function fadeOut(){
+    setInterval(loader,1000)
+}
+window.onload = fadeOut();
